a056177309c518b98417766392fdcd742b9be754
mikle <mikle>
2024-06-24 19:10:38 +0300

    Spring Data JPA

--- src/main/java/ru/job4j/social/media/api/repository/FriendRepository.java
+++ src/main/java/ru/job4j/social/media/api/repository/FriendRepository.java
@@ -4,5 +4,5 @@ package ru.job4j.social.media.api.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.social.media.api.model.Friend;
 
-public interface FriendRepository extends CrudRepository<Friend, Integer> {
+public interface FriendRepository extends CrudRepository<Friend, Long> {
 }
