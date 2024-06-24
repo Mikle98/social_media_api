826f5f3c346354b747faba0e0fb32c10705cbe8e
mikle <mikle>
2024-06-24 19:30:32 +0300

    Именованные запросы

--- src/main/java/ru/job4j/social/media/api/repository/PostRepository.java
+++ src/main/java/ru/job4j/social/media/api/repository/PostRepository.java
@@ -1,7 +1,18 @@
 package ru.job4j.social.media.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.social.media.api.model.Post;
+import ru.job4j.social.media.api.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
+    public List<Post> findByUser(User user);
+
+    public List<Post> findByCreatedGreaterThanEqualAndCreatedLessThanEqual(LocalDateTime createdStart, LocalDateTime createdEnd);
+
+    public Page<Post> findByOrderByCreatedDesc(Pageable pageable);
 }
